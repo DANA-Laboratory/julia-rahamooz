544c1a9c644931d663b0e15fb61616aa8ed8c8ce
Reza Afzalan <RAfzalan@gmail.com>
2015-09-06 15:51:31 +0430

    ed no4

--- src/juliaRahamooz.jl
+++ src/juliaRahamooz.jl
@@ -1,3 +1,4 @@
 include("no1.jl");
 include("no2.jl");
 include("no3.jl");
+include("no4.jl");
--- src/no4.jl
+++ src/no4.jl
@@ -3,17 +3,29 @@
 علاوه بر این توابع پایه، مجموعه کاملتری از قابلیت ها در پکیجهای جانبی جولیا تعریف شده اند، فهرست مهمترین پکیجهای تخصصی ی آمار و احتمالات را میتوان با مراجعه به آدرس https://github.com/JuliaStats دریافت نمود. 
 در ارائه مثالهای این یادداشت فرض براین است که خواننده از نحوه کار با 1- بردارها و آرایه ها در جولیا و 2- ماکروهای تست (Unit and Functional Testing) اطلاع دارد.
 !=#
-
+using Base.Test
 #=!
 داده های آماری را بصورت برداری از اعداد تصادفی تولید مینماییم
 !=#
-using Base.Test #=> مجموعه توابع تست استفاده گردند
-randomdata = rand(100); #=> 100-element Array{Float64,1}
-meanval = mean(randomdata) #=> تابع mean مقدار متوسط داده ها را محاسبه میکند
+randomdata = rand(100); # => 100-element Array{Float64,1}
+#=!
+تابع mean مقدار متوسط داده ها را محاسبه میکند
+!=#
+meanval = mean(randomdata) 
 @test_approx_eq meanval sum(randomdata)/length(randomdata)
-varval = std(randomdata) #=> تابع var وارینس داده ها را محاسبه میکند 
+#=!
+تابع var وارینس داده ها را محاسبه میکند 
+!=#
+varval = var(randomdata) 
 @test_approx_eq varval sum((randomdata - meanval).^2) / (length(randomdata) - 1)
-stdval = std(randomdata) #=> تابع std انحراف از معیار داده های را محاسبه میکند 
+#=!
+تابع std انحراف از معیار داده های را محاسبه میکند
+!=#
+stdval = std(randomdata)
 @test_approx_eq stdval sqrt(varval)
-midval = middle(randomdata) #=> تابع middle مقدار میانی را محاسبه میکند
-@test_approx_eq midval (max(randomdata)-min(randomdata))/2 
+#=!
+تابع middle مقدار میانی را محاسبه میکند
+!=#
+midval = middle(randomdata) 
+@test_approx_eq midval (maximum(randomdata)+minimum(randomdata))/2
+ 
