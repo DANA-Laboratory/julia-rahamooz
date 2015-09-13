85994e91d09eb984b469354cb1ddd0b1ab9495d1
Reza Afzalan <RAfzalan@gmail.com>
2015-09-13 08:14:14 +0430

    no5

--- src/juliaRahamooz.jl
+++ src/juliaRahamooz.jl
@@ -2,3 +2,5 @@ include("no1.jl");
 include("no2.jl");
 include("no3.jl");
 include("no4.jl");
+cd(Pkg.dir(joinpath("juliaRahamooz","src")));
+run(`julia --track-allocation=user --code-coverage=user -e "include(\"no5.jl\"); using Base.Test; @test fun(100) == 101"`) 
