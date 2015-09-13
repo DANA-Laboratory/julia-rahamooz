e32baeb2b3477900366f75d7c46c8c7cbf856268
Reza Afzalan <rafzalan@gmail.com>
2015-09-13 10:40:49 +0000

    Update juliaRahamooz.jl

--- src/juliaRahamooz.jl
+++ src/juliaRahamooz.jl
@@ -3,4 +3,4 @@ include("no2.jl");
 include("no3.jl");
 include("no4.jl");
 cd(Pkg.dir(joinpath("juliaRahamooz","src")));
-run(`julia --track-allocation=user --code-coverage=user -e "include(\"no5.jl\"); using Base.Test; @test vectorizeVSdevectorize() "`) 
+run(`julia --track-allocation=user --code-coverage=user -e "include(\"no5.jl\"); using Base.Test; @test vecORdevec() "`) 
